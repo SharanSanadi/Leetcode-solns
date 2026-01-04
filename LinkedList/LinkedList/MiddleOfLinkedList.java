c74a2d413c00ca213e849b0ee4a1df364cd59ee6
SharanSanadi <1nt24cs258.sharan@nmit.ac.in>
2026-01-04 12:50:00 +0530

    Add solution for finding middle of linked list

---
+++ LinkedList/LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,14 @@
+// LeetCode 876: Middle of the Linked List
+// Time: O(n), Space: O(1)
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
