f03b6b416dd8d2f819a72f1bf86ef5848b0ed9ee
SharanSanadi <1nt24cs258.sharan@nmit.ac.in>
2026-01-13 12:24:58 +0530

    Implement Way Too Long Words solution

---
+++ Codeforce/WayTooLongWords_71A.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n-- > 0) {
+            String s = sc.next();
+            int len = s.length();
+
+            if (len > 10) {
+                System.out.println(
+                    s.charAt(0) + "" + (len - 2) + s.charAt(len - 1)
+                );
+            } else {
+                System.out.println(s);
+            }
+        }
+    }
+}
