e49fcadd923fc93b274b5d28e8e7f1cd9204d160
SharanSanadi <1nt24cs258.sharan@nmit.ac.in>
2026-01-17 17:29:31 +0530

    Add Solution class to check if a number has three divisors

---
+++ MATH/ThreeDivisors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isThree(int n) {
+        int root = (int) Math.sqrt(n);
+        if (root * root != n) return false;
+
+        return isPrime(root);
+    }
+
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
+ 
